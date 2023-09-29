783aa3f5c68fab40b494957f29f2e3495f48029b
WLKRE <wlkrstudios@hotmail.com>
2023-09-29 17:42:57 -0400

    Fixed lag involving util.IsValidModel and older computers.

--- lua/autorun/mp_playermodel.lua
+++ lua/autorun/mp_playermodel.lua
@@ -1,19 +1,32 @@
 
+local playerModelSupport = nil;
+
 hook.Add("PlayerPostThink", "POWERSUIT.PlayerModels", function(ply)
 
-	if (!tobool(ply:GetInfo("mp_options_playermodel"))) then return; end
+	-- PlayerModelSupport is a three state variable. Nil means we haven't checked if the models are present.
+	-- False means the models are not present. True means the models are installed.
+	if (playerModelSupport == false || !tobool(ply:GetInfo("mp_options_playermodel"))) then return; end
 
+	-- Attempt to get powersuit data.
 	local isPowerSuit, weapon = ply:UsingPowerSuit();
 	if (!isPowerSuit) then return; end
 
 	-- Avoid running hook if suit did not change.
 	local suit, suitID = weapon:GetSuit();
-	if (weapon.Suit == suitID || !util.IsValidModel(suit.WorldModel)) then return; end
+	if (weapon.Suit == suitID) then
+		return;
+	else
+		playerModelSupport = nil;
+		weapon.Suit = suitID;
+	end
+
+	-- Check for player model support.
+	if (playerModelSupport == nil) then playerModelSupport = util.IsValidModel(suit.WorldModel); end
+	if (!playerModelSupport)       then return; end
 
 	-- Apply suit model, skin and bodygroups to player now.
 	WGL.ForceSetModel(ply, suit.WorldModel);
 	WGL.SetBodyGroupSkin(ply, 1, suit.Group, suit.Skin);
-	weapon.Suit = suitID;
 end);
 
 hook.Remove("StartCommand",       "morphball");
